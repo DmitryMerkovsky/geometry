7dfe4ec893c00c87d1a8ac9c13b7c3c684c61be1
Мелешков Дмитрий Павлович <ip811s18@mwp11.csc.local>
2019-03-20 10:55:33 +0700

    Make circle output function

---
+++ circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void Circle(float koordx, float koordy, float radius){
+ float P,S;
+P=2*3.14*radius;
+S=3.14*radius*radius;
+printf("Perimeter=%f \nArea=%f\n",P,S);}
+int main(){
+float x, y, R;	
+printf("Input x, y, radius:"); 
+scanf("%f %f %f",&x,&y,&R);
+ Circle (x,y,R);
+ return 0;
+}
