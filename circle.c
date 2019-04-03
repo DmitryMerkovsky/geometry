d5c679da1b7349f1e43a21ebbbf0b344fe34e649
Мелешков Дмитрий Павлович <ip811s18@mwp11.csc.local>
2019-04-03 11:22:24 +0700

    Format circle.c with codestyle

--- circle.c
+++ circle.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-void Circle(float koordx, float koordy, float radius){
- float P,S;
-P=2*3.14*radius;
-S=3.14*radius*radius;
-printf("Perimeter=%f \nArea=%f\n",P,S);}
-int main(){
-float x, y, R;	
-printf("Input x, y, radius:"); 
-scanf("%f %f %f",&x,&y,&R);
- Circle (x,y,R);
- return 0;
+void Circle(float koordx, float koordy, float radius)
+{
+    float P, S;
+    P = 2 * 3.14 * radius;
+    S = 3.14 * radius * radius;
+    printf("Perimeter=%f \nArea=%f\n", P, S);
+}
+int main()
+{
+    float x, y, R;
+    printf("Input x, y, radius:");
+    scanf("%f %f %f", &x, &y, &R);
+    Circle(x, y, R);
+    return 0;
 }
