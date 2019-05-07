305712637b714731c2b4f2b83c2385ba08935570
DmitryMerkovsky <amnessionalt@gmail.com>
2019-05-08 04:41:05 +0700

    triangle and circle function implement

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 void Circle(float koordx, float koordy, float radius)
 {
     float P, S;
@@ -6,11 +9,29 @@ void Circle(float koordx, float koordy, float radius)
     S = 3.14 * radius * radius;
     printf("Perimeter=%f \nArea=%f\n", P, S);
 }
+void Triangle(float length1, float length2, float length3)
+{
+	float P, S;
+	P = length1+length2+length3;
+	float p = P/2;
+	S = sqrt(p*(p-length1)*(p-length2)*(p-length3));
+	printf("Perimeter=%f \nArea=%f\n", P, S);
+}
 int main()
 {
-    float x, y, R;
-    printf("Input x, y, radius:");
-    scanf("%f %f %f", &x, &y, &R);
-    Circle(x, y, R);
+    char figure[16];
+    printf("Input figure:");
+    scanf("%s",figure);
+    if (strcmp (figure, "circle")==0)
+    
+    Circle(1.2,2,4.3);
+    	
+    else if (strcmp (figure, "triangle")==0)
+    	
+    Triangle(2.05,1.5,3);
+	      
+	else printf("No such figure");
+	
+	
     return 0;
 }
